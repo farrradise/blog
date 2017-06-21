686561404b87dd40b45b77cb59b5801833ec8402
farrradise <marzini.f@gmail.com>
2017-06-21 13:59:44 +0200

    js : added tabs

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ function confirmComm(th) {
 
 
 // FONCTION POUR TABS
-/*
+
 function testTab(evt, laTab) {
     // Declare all variables
     var i, tabcontent, tablinks;
@@ -32,17 +32,10 @@ function testTab(evt, laTab) {
         tabcontent[i].style.display = "none";
     }
 
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-
     // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(laTab).style.display = "block";
     evt.currentTarget.className += " active";
 }
-*/
 
 
 
